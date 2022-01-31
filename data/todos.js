d7b94603eb245bf623d865a1f849614fe19475d9
EmmaNGagen <emma.n.gagen@outlook.com>
2022-01-31 13:08:03 +0100

    first commit

---
+++ data/todos.js
@@ -0,0 +1,8 @@
+const todos = [
+  { id: 1, created: "20/1 09:00", task: "Morgonmöte jobb ", done: false },
+  { id: 2, created: "20/1 15:20", task: "Skicka in budget", done: false },
+  { id: 3, created: "22/1 08:15", task: "Yoga på Stc", done: false },
+  { id: 4, created: "25/1 11:30", task: "Lunch med jobbet", done: false },
+];
+
+module.exports = todos;
---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const todos = require("./data/todos.js");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
+
+const app = express();
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+function getNewId(list) {
+  let maxId = 0;
+  for (const item of list) {
+    if (item.id > maxId) {
+      maxId = item.id;
+    }
+  }
+  return maxId + 1;
+}
+
+app.get("/", (req, res) => {
+  res.render("home", { todos });
+});
+
+app.get("/home/ny", (req, res) => {
+  res.render("home");
+});
+
+app.post("/home/ny", (req, res) => {
+  const id = getNewId(todos);
+  const newTask = {
+    id: id,
+    created: req.body.created,
+    task: req.body.task,
+    done: req.body.done,
+  };
+
+  todos.push(newTask);
+
+  res.redirect("/uppgift/" + id);
+});
+
+app.get("/homec/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+  res.render("single-task", todo);
+});
+
+app.get("/uppgift/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+  res.render("single-task", todo);
+});
+
+app.get("/uppgift/:id/redigera", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+
+  res.render("edit-task", todo);
+});
+
+app.post("/uppgift/:id/redigera", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((t) => t.id === id);
+
+  todos[index].created = req.body.created;
+  todos[index].task = req.body.task;
+  todos[index].done = req.body.task;
+
+  res.redirect("/uppgift/" + id);
+});
+
+app.get("/uppgift/:id/ta-bort", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+
+  res.render("delete-task", todo);
+});
+//Id i url måste vi alltid hämta med parseInt(req.params.id)
+app.post("/uppgift/:id/ta-bort", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((t) => t.id === id);
+
+  todos.splice(index, 1);
+
+  res.redirect("/");
+});
+
+app.listen(8000, () => {
+  console.log("http://localhost:8000/");
+});
